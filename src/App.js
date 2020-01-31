a87d102de24a14bd3277e81e572aa22d73eac979
Ike Otu <ikeotu@msn.com>
2020-01-31 12:40:37 -0800

    LocationList component done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header.js";
 import {Route} from "react-router-dom";
 import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
 
 
 
@@ -13,6 +14,10 @@ export default function App() {
 
       <Route exact path="/">
         <WelcomePage />
+      </Route>
+
+      <Route path="/characters">
+        <CharacterList />
       </Route>
 
     </main>
