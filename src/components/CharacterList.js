1c99d8409b6e40904e766c8eee64afb0688adc2d
Ike Otu <ikeotu@msn.com>
2020-01-31 11:42:00 -0800

    characterList component done

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import Search from './SearchForm';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 const [search, setSearch] = useState('');
-const [data, setData] = useState([]);
+const [results, setResults] = useState([]);
   useEffect(() => {
     axios
       .get('https://rickandmortyapi.com/api/character/')
          .then(res => {
-           console.log('RES', res)
+           let characters = res.data.results.filter(char => {
+             return char.name.toLowerCase().includes(search.toLowerCase());
+           })
+
+           setResults(characters);
          })
          .catch(error => {
            console.log("Sorry nothing returned", error);
          })
-  }, []); 
+  }, [search]); 
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <Search search={search} setSearch={setSearch}/> 
+    
+
+      <cardContainer>
+        {results.map(char => {
+          return (
+            <CharacterCard character={char} />
+          )
+        })}
+
+      </cardContainer>
     </section>
   );
 }
